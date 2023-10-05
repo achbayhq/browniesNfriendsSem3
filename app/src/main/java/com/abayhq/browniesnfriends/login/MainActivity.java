7185bc1a133cea1bfdad2b6bf34dd92e901d467b
achbayhq <achmadbayhaqiprog@gmail.com>
2023-10-05 09:45:13 +0700

    First commit

---
+++ app/src/main/java/com/abayhq/browniesnfriends/login/MainActivity.java
@@ -0,0 +1,16 @@
+package com.abayhq.browniesnfriends.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.abayhq.browniesnfriends.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
