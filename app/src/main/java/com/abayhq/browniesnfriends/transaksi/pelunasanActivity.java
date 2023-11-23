3fd9167c50b9b56aeb6d825d1725924c16741a84
anantaghaisani <anantaghaisani@gmail.com>
2023-11-23 16:04:47 +0700

    pelunasan
    xml, activity

---
+++ app/src/main/java/com/abayhq/browniesnfriends/transaksi/pelunasanActivity.java
@@ -0,0 +1,16 @@
+package com.abayhq.browniesnfriends.transaksi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.abayhq.browniesnfriends.R;
+
+public class pelunasanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pelunasan);
+    }
+}
